38e9f898e7197b07c5c30c78e2991ac1cad512ba
netheadpro <nethead.pro@gmail.com>
2016-11-30 20:14:20 +0530

    Update blogtoc_orig.js

--- blogtoc_orig.js
+++ blogtoc_orig.js
@@ -1,16 +1,3 @@
-
-
-
-
-
-/*
-     FILE ARCHIVED ON 15:21:13 Apr 7, 2016 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 14:40:00 Nov 30, 2016.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
 // ---------------------------------------------------
 // BLOGTOC
 // ---------------------------------------------------
